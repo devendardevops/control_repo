ec0266269129b657fe39562a870b8dfc129b2a25
Devendar <82363435+devendardevops@users.noreply.github.com>
2021-05-28 17:18:07 +0530

    Update site.pp
    removed all the previous code from the file

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default{
-  file{'/root/README':
-    ensure => file,
-    content => 'this is the readme file',
-    owner => 'root',
-  }
-  file{'/root/README':
-    owner => 'root',
-  }
+  
 }
