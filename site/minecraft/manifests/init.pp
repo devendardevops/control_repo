fd60b672578a66df98ebc781e2fccc8bcdba2d33
Devendar <82363435+devendardevops@users.noreply.github.com>
2021-06-07 12:14:52 +0530

    Update init.pp
    included eula

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -12,6 +12,11 @@ class minecraft{
     ensure => present,
   }
   
+  file{ '/opt/minecraft/eula.txt':
+    ensure => file,
+    content => "eula=true",
+   }
+  
   file{'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
