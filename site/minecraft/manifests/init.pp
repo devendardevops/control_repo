c2de7c3a084bfa47370d711c34bcc95e76b170e6
Devendar <82363435+devendardevops@users.noreply.github.com>
2021-06-07 12:34:39 +0530

    Update init.pp
    included parameters

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,11 +1,14 @@
-class minecraft{
-  file{'/opt/minecraft':
+class minecraft(
+$url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+$install_dir = '/opt/minecraft'
+){
+  file{$install_dir:
     ensure => directory,
   }
   
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {"${install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    source => $url,
     before => Service['minecraft'],
   }
   
@@ -13,7 +16,7 @@ class minecraft{
     ensure => present,
   }
   
-  file{ '/opt/minecraft/eula.txt':
+  file{ "${install_dir}/eula.txt":
     ensure => file,
     content => "eula=true",
    }
@@ -26,6 +29,6 @@ class minecraft{
   service{'minecraft':
     ensure => running,
     enable => true,
-    require => [package['java'],file['/opt/minecraft/eula.txt'],file['/etc/systemd/system/minecraft.service']],
+    require => [package['java'],file["${install_dir}/eula.txt"],file['/etc/systemd/system/minecraft.service']],
   }
 }
